9906773260ce1056d0d2ae134d019085ba1bc13b
KTurbanski <krzysztofturbanski@gmail.com>
2017-09-12 12:13:49 +0200

    Zajęcia 12.09.2017

--- JS/funkcje.js
+++ JS/funkcje.js
@@ -1,3 +1,4 @@
+/*
 function witaj(imie){
     document.write("Witaj " + imie);
 }
@@ -12,5 +13,44 @@ function poleProstokata(a, b){
 poleProstokata(a, b);
 
 var name = 'Janusz';
-witaj(name);
+//witaj(name);
+*/
+
+
+function poleobjetosc(szerokosc,dlugosc,wysokosc){
+    var pole = szerokosc * dlugosc;
+    var objetosc = pole * wysokosc;
+    var wynik = [pole, objetosc];
+    return wynik;
+}
+
+/*var poleProstokata = poleobjetosc(2,3,4)[0];
+console.log(poleProstokata);*/
+
+/*var a = prompt("Podaj szerokość");
+var b = prompt("Podaj dlugosc");
+var c = prompt("Podaj szerokość");
+var x = prompt("Podaj wartość", "0 - Pole, 1 - objętość")
+var poleProstokata = poleobjetosc(a,b,c)[x];
+console.log(poleProstokata);*/
+
+// Napisać program który oblicza obwód, pole koła oraz objętość stożka
+// Dane użytkownik podaje z klawiatury, wykorzystaj obiekt math
+// Użytkownik wybiera za pomocą pola radio
+// Dane wprowadza w formularzu
+// Dane wyświetl w bloku strony
+
+var a = ;
+var b = ;
+
+
+// 2pr
+// pr2
+
+
+
+
+
+
+
 
